ef959bd065caa78bb16ddf56a6905a4f84567a54
Mashebasep <mashebasep@gmail.com>
2025-09-20 10:41:38 -0700

    Add match case logic

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,42 @@
 # get inputs
-num1 = float(input("Enter the first number: "))
+num1 = float(input("Enter the first number:" ))
 num2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
-# Perform calculation using match case
-if operation == "+":
-    print(f"The result is {num1 + num2}. ")
-elif operation == "-":
-    print(f"The result is {num1 - num2}. ")
-elif operation == "*":
-    print(f"The result is {num1 * num2}. ")
-elif operation == "/":
-    if num2 == 0:
-        print("Cannot divide by zero.")
-    else:
-        print(f"The result is {num1 / num2}.")
-elif operation not in ("+", "-", "*", "/"):
-    print("Invalid operation selected.")
+# Match case logic
+match operation:
+    case "+":
+        print(f"The result is {num1 + num2}")
     
+    case "-":
+        print(f"The result is {num1 - num2}")
+
+        match operation:
+            case "*":
+                print(f"The result is {num1 * num2}")
+                match operation:
+                    case "/":
+                        if num2 == 0:
+                            print("Cannot divide by zero.")
+                        else:
+                            print(f"The result is {num1 / num2}")
+                            case
+                            print("Invalid operation selected.")
+                            
+
+
+
+
+        
+        
+        
+
+
+        
+        
+
+
+
+
 
 
     
