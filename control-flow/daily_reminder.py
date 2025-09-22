200f0cd53a063a354588e9780e5888ff2b14f52b
Mashebasep <mashebasep@gmail.com>
2025-09-22 11:15:06 -0700

    Update daily_reminder.py in control-flow

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,23 +5,20 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 # Match Case for priority + time-bound
 match priority:
     case "high":
-        reminder_message = f"'{task}' is a **high** priority task"
+        priority_level = "high"
     case "medium":
-        reminder_message = f" '{task}' is a **medium** priority task"
+        priority_level ="medium"
     case "low":
-        reminder_message = f"'{task}' is a **low** priority task. consider completing it when you have free time"
+        priority_level ="low"
     case _:
-        reminder_message = f"'{task}' is a task with an **Unspecified** priority"
+        priority_level = "unknown"
 
-        if time_bound == "yes" and priority in ["high", "medium"]:
-            remider_message = f"{reminder_message} that requires immediate attention today!"
-        elif time_bound == "yes" and priority == "low":
-            reminder_message = f"'{task}' is a **low** priority task, but it requires attention today!"
-        elif time_bound == "no" and priority in ["high","medium"]:
-            reminder_message = f"(reminder_message)."
-
-            print(f"/nReminder: {reminder_message}")
+        reminder = f"Reminder: '{task}' has ({priority_level}) priority."
+        if time_bound == "yes"and priority == "high":
+            reminder += " that requires immediate attention today!"
+            print(reminder)
             
+
 
 
 
