56ce53128933518a41ee257de22d23c8fc3c3d7a
Mashebasep <mashebasep@gmail.com>
2025-09-26 09:11:15 -0700

    Add perform basic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,7 @@ def perform_operation(num1: float, num2: float, operation: str):
     # Perform basic arithmetic operations.
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").lower()
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
