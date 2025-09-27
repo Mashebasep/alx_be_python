a6c05016ae862c7587ead90ffcbb7857077a7a5c
Mashebasep <mashebasep@gmail.com>
2025-09-27 01:59:23 -0700

    Add shopping list manager and enter input

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -33,15 +33,15 @@ def display_menu():
                                     shopping_list = []
                                     while True:
                                         display_menu()
-                                        # Asking for a number as input       
-                                        num = float(input("Enter a number: "))
-                                        if num == 1:
+                                        # Aking for input       
+                                        choice = input("Enter your choice: ")
+                                        if choice == 1:
                                             add_item(shopping_list)
-                                        elif num == 2:
+                                        elif choice == 2:
                                             remove_item(shopping_list)
-                                        elif num == 3:
+                                        elif choice == 3:
                                             view_list(shopping_list)
-                                        elif num == 4:
+                                        elif choice == 4:
                                             print("Goodbye!")
                                             break
                                         else:
