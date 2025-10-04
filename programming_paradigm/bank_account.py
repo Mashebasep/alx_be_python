2fe197b34a530d326d5ebed506cce440b2a72a48
Mashebasep <mashebasep@gmail.com>
2025-10-04 12:02:56 -0700

    Add bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,16 +3,53 @@ class BankAccount:
         self.__account_balance = initial_balance
 
         def deposit(self, amount):
-            self.__account_balance += amount
+            if amount > 0:
+                 self.__account_balance += amount
+                 print(f"Deposited ${amount:.2F}. New balance is ${self.__accout_balance:.2F}")
+            else:
+                 print("Invalid deposit amount.")
 
-            def withdrawl(self, amount):
-                    if amount > self.__account_balance:
+
+            def withdraw(self, amount):
+                    if 0 < amount <=self.__account_balance:
+                        self.__account_balance -= amount
+                        print(f"Withdrew ${amount:.2F}. New balance is ${self.__account_balance:.2F}") 
+                        return True
+                    elif amount <= 0:
+                        print("Invalid withdrawal amount.")
                         return False
-                    self.__account_balance -= amount
-                    return True
-            
+                    else:
+                        print("Insufficient funds.")
+                        return False
             def display_balance(self):
                  print(f"Current balance: ${self.__account_balance:.2F}")
-                 
+                 def main():
+                      account = BankAccount(1000)
+                      while True:
+                           print("/nBanking Menu:")
+                           print("1. Deposit")
+                           print("2. Withdraw")
+                           print("3. Display Balance")
+                           print("4. Exit")
+                           choice = input("Enter your choice:")
+                           if choice == '1':
+                                amount = float(input("Enter amount to deposit: "))
+                                account.deposit(amount)
+                           elif choice == '2':
+                                amount = float(input("Enter amount to withdraw: "))
+                                account.withdraw(amount)
+                           elif choice == '3':
+                                account.display_balance()
+                           elif choice == '4':
+                                print("Exiting program.")
+                                break
+                           else:
+                                print("Invalid choice. Please try again.")
+                                
+                                
+
+                                     
+
+
 
 
