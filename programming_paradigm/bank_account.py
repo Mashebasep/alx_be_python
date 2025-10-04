308a536e7440e06c47f6ca699f653650b1ea9868
Mashebasep <mashebasep@gmail.com>
2025-10-04 12:54:56 -0700

    Add bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,11 +1,18 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
         self.__account_balance = initial_balance
-        def withdraw(self, amount):
+
+        def deposit(self, amount):
+             self.account_balance += amount
+             print("Deposit:" , amount)
+
+        def withdraw(self, amount): 
                 if amount <= self.account_balance:
                     self.account_balance -= amount
+                    print("Withdraw:"  , amount)
                     return True
                 else:
+                    print("Insufficient funds.")
                     return False
                 
                 def display_balance(self):
