22c19ee0ffa445a01080ceab71c6756b4ab94d4f
Mashebasep <mashebasep@gmail.com>
2025-10-04 13:11:49 -0700

    Add bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance
+    def __init__(self):
+        self.balance = 0
 
         def deposit(self, amount):
              self.account_balance += amount
