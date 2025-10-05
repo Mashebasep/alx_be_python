bc31317a65c3b89749004ca83886a98c9143ad66
Mashebasep <mashebasep@gmail.com>
2025-10-05 01:35:21 -0700

    Add the specified, deduct the amount, print the amount,print the current balance and instance test

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,26 +1,34 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
-
+# Add the specified amount
         def deposit(self, amount):
              self.account_balance += amount
-             print(f"Deposited: ${amount}")
+             print(f"Deposited: ${amount:.2F}")
              print(f"Current balance: ${self.account_balance}")
              self.display_balance()
-
+# Deduct the amount
         def withdraw(self, amount): 
-                if amount <= self.account_balance:
+                if amount > self.account_balance:
+                    print("Insufficient funds.")
+                    return False
+                
                     self.account_balance -= amount
-                    print(f"Withdrew: ${amount}")
+                    print(f"Withdrew: ${amount:.2F}")
                     print(f"Current balance: ${self.account_balance}")
-                    self.display_balance()
                     return True
-
-                # Display
+                # Print the current balance
                 def display_balance(self):
-                    print(f"Current Balance: ${self.accout_balance}")
-                    print(f"Deposited amount: ${amount}")
-                    print(f"Withdrew amount: ${amount}")
+                    print(f"Current Balance: ${self.accout_balance:.2F}")
+                    # Create an instance to test
+                    account = BankAccount(100)
+                    account.display_balance()
+                    account.deposit(50)
+                    account.display_balance()
+                    account.withdraw(20)
+                    account.display_balance()
+                    
+
                     
 
 
