94acb693b265316f9cfcb82bdae456e9c3dbb082
Mashebasep <mashebasep@gmail.com>
2025-10-12 11:00:35 -0700

    Add Polymorphism demo

--- oop/main.py
+++ oop/main.py
@@ -1,7 +1,6 @@
-from polymorphism_demo import Shape, Rectangle, Circle
-import math
+from polymorphism_demo import Rectangle, Circle
 def main():
     shapes = [Rectangle(10, 5), Circle(7)] 
     for shape in shapes:
         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
-        
+
--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,15 +1,21 @@
 import math
 class Shape:
-    def area(self):
-        class Rectangle(Shape):
-            def __init__(self, length, width):
-                self.length = length
-                self.width = width
-                def area(self):
-                    return self.length * self.width
-                class Circle(Shape):
-                    def __init__(self, radius):
-                        self.radius = radius
-                        def area(self):
-                            return math.pi * (self.radius ** 2)
-                        
+      def area(self):
+          class Rectangle(Shape): 
+               def __init__(self, length, width):
+                   self.length = length
+                   self.width = width
+                   def area(self):
+                        return self.length * self.width
+                   class Circle(Shape):
+                        def __init__(self, radius):
+                             self.radius= radius
+                             def area(self):
+                                  return math.pi * (self.raidus ** 2)
+                             def main():
+                                  shapes = [Rectangle(10, 5), Circle(7)]
+                                  for shape in shapes:
+                                       print(f"The area of the {shape.__class__.name__} is: {shape.area()}")
+                                       
+                             
+                             
