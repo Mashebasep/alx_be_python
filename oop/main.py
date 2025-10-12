b00c39f46b7378a0ceb5d64f6ab0735d8f7f7d04
Mashebasep <mashebasep@gmail.com>
2025-10-12 05:57:15 -0700

    Add Book class and main.py

--- oop/main.py
+++ oop/main.py
@@ -1,6 +1,6 @@
 from book_class import Book
 def main():
-    my_book = Book("1984", "George Orwell", 1949)
+    my_book = Book("1984", "by George Orwell", "published in 1949")
     #  Uses __str__
     print(my_book)
     # Uses __repr__
