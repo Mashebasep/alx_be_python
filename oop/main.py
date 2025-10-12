715012ca097fe001d83f62c5c5e032863be857a5
Mashebasep <mashebasep@gmail.com>
2025-10-12 06:44:16 -0700

    Add Book class and main.py

--- oop/main.py
+++ oop/main.py
@@ -7,7 +7,11 @@ def main():
     print(my_book)
     del my_book
     if __name__ == "__main__":
-        main()
+        my_book = Book("1984", "George Orwell", 1949)
+        print(my_book)
+        print(repr(my_book))
+        del my_book
+        
 
 
 
