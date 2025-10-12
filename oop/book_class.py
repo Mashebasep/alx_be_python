f9d015c41e0694d12082587314f9062656e81b65
Mashebasep <mashebasep@gmail.com>
2025-10-12 05:38:29 -0700

    Add book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,16 +4,18 @@ class Book:
         self.author = author
         self.year = year
         def __str__(self):
-                return f"{self.title} by {self.author}, published in {self.year}"
-        def __repr__(self):
-                return f"Book('{self.title}', '{self.author}', {self.year})"
+                return f"{self.title}, {self.author}, {self.year}"
         def __del__(self):
-            print(f"Deleting {self.title}")
-            # Test to verify output
-            my_book = Book("1984", "George Orwell", 1949)
-            print(my_book)
-            print(repr(my_book))
-            del my_book
+              print(f"Deleting {self.title}")
+        def __repr__(self):
+                return f"Book '{self.title}', '{self.author}',{self.year})"
+        def __str__(self):
+              return f"{self.title}, {self.author}, {self.year}"
+        my_book =Book("1984", "George Orwel", "1949")
+        print(my_book)
+
+
+
 
 
 
