3d2ee819dd3ac84c4e4cf25010638155b3dccedf
Mashebasep <mashebasep@gmail.com>
2025-10-12 05:01:26 -0700

    Add main.py and print method

--- oop/main.py
+++ oop/main.py
@@ -2,8 +2,9 @@ from book_class import Book
 def main():
     my_book = Book("1984", "George Orwell", 1949)
     # Print method
-    print("1984 George Orwell, 1949")
-    print(repr(my_book))
+    print(my_book.__str__())
+    print(my_book.__repr__())
     del my_book
+    
 
     
