5372861471509a3acafe96a1399f4cddc95002ba
Mashebasep <mashebasep@gmail.com>
2025-10-12 12:22:22 -0700

    Add main.py

--- oop/main.py
+++ oop/main.py
@@ -1,5 +1,8 @@
 from polymorphism_demo import Shape,Rectangle, Circle
 def main():
+    shapes = [Rectangle(10, 5, Circle(7))]
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
     rectangle = Rectangle(10, 5)
     circle = Circle(7)
     print(f"The area of the Rectangle is: {rectangle.are()}")
